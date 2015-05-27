ffeea1aaf51ad4dbf416a0e674f4cae5f813d73e
Shannon Smith <shannones2013@gmail.com>
2015-05-27 15:38:56 -0700

    html,css, page1&2

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+   	$(".start").click(function(){
+   			$(".title").hide();
+   			$(".question").show();
+   			$(".start").hide();
+   			$(".next").show();
+   			$("form").show();
+   		});
+ });
