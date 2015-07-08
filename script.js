b7bee1f2b8d36d0fd202f8138c5a2045494cb049
Shannon Smith <shannones2013@gmail.com>
2015-07-07 17:49:42 -0700

    arrays in object

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
- var quiz = {
-   one: "The avarage pregnancy is __ weeks.",
-   two: "How many more calories should a pregnant women consume?",
-   three: "Which position should you not sleep while pregnant?",
-   four: "What foods should you avoid while pregnant?"
-};
-var answerOne = ["35", "37", "40", "42"];
-var answerTwo = ["300-500", "100-300", "1,000-1,200","600-800" ];
-var answerThree = ["Back", "Stomach", "Left side", "Right side"];
-var answerFour = ["Raw", "Organic", "Local", "All of the above"];    
-
 $( document ).ready(function() {
+   //quiz array
+   var quiz = [ {
+      question: "The avarage pregnancy is __ weeks.",
+      choices: ["35", "37", "42", "40"],
+      correct: 3
+   },
+   {
+      question: "How many more calories should a pregnant women consume?",
+      choices: ["300-500", "100-300", "1,000-1,200","600-800" ],
+      correct: 1
+   },
+   {
+      question: "Which position should you not sleep while pregnant?",
+      choices: ["Back", "Stomach", "Left side", "Right side"],
+      correct: 0
+   },
+   {
+      question: "What foods should you avoid while pregnant?",
+      choices: ["Local", "Organic", "Raw", "All of the above"]
+      correct: 2
+   }];
+   // functions
    $(".app .next.question-1").on("click",function(){
          $(".question").show();
-         $(".app .question").text(quiz.one);
+         $(".app .question").text(quiz[0].question);
          $(".title").hide();
          $("form").show();
-         $(".app .answer-a").text(answerOne[0]);
-         $(".app .answer-b").text(answerOne[1]);         
-         $(".app .answer-c").text(answerOne[2]);
-         $(".app .answer-d").text(answerOne[3]);
+         $(".app .answer-a").text(quiz[0].choices[0]);
+         $(".app .answer-b").text(quiz[0].choices[1]);         
+         $(".app .answer-c").text(quiz[0].choices[2]);
+         $(".app .answer-d").text(quiz[0].choices[3]);
          $(".app .next.question-2").show();
    });
    $(".app .next.question-2").on("click",function(){
-      $(".app .question").text(quiz.two);
-      $(".app .answer-a").text(answerTwo[0]);
-      $(".app .answer-b").text(answerTwo[1]);         
-      $(".app .answer-c").text(answerTwo[2]);
-      $(".app .answer-d").text(answerTwo[3]);
+      $(".app .question").text(quiz[1].question);
+      $(".app .answer-a").text(quiz[1].choices[0]);
+      $(".app .answer-b").text(quiz[1].choices[1]);         
+      $(".app .answer-c").text(quiz[1].choices[2]);
+      $(".app .answer-d").text(quiz[1].choices[3]);
       $(this).hide();
       $(".app .next.question-3").show();
    });
      $(".app .next.question-3").on("click",function(){
-      $(".app .question").text(quiz.three);
-      $(".app .answer-a").text(answerThree[0]);
-      $(".app .answer-b").text(answerThree[1]);         
-      $(".app .answer-c").text(answerThree[2]);
-      $(".app .answer-d").text(answerThree[3]);
+      $(".app .question").text(quiz[2].question);
+      $(".app .answer-a").text(quiz[2].choices[0]);
+      $(".app .answer-b").text(quiz[2].choices[1]);         
+      $(".app .answer-c").text(quiz[2].choices[2]);
+      $(".app .answer-d").text(quiz[2].choices[3]);
       $(this).hide();
       $(".app .next.question-4").show();
    });
        $(".app .next.question-4").on("click",function(){
-      $(".app .question").text(quiz.four);
-      $(".app .answer-a").text(answerFour[0]);
-      $(".app .answer-b").text(answerFour[1]);         
-      $(".app .answer-c").text(answerFour[2]);
-      $(".app .answer-d").text(answerFour[3]);
+      $(".app .question").text(quiz[3].question);
+      $(".app .answer-a").text(quiz[3].choices[0]);
+      $(".app .answer-b").text(quiz[3].choices[1]);         
+      $(".app .answer-c").text(quiz[3].choices[2]);
+      $(".app .answer-d").text(quiz[3].choices[3]);
       $(this).hide();
       $(".app .next.finish").show();
-   });
-     
-
-
+   }); 
 });
